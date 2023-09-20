ea323993a3503525ad9906e54ab76d97664e40dd
abishevs <abisevs.eduards@gmail.com>
2023-09-20 17:55:32 +0200

    feat: added inline erros from lsp server

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -33,6 +33,16 @@ lsp.configure('lua_ls', {
     }
 })
 
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
@@ -45,7 +55,10 @@ lsp.on_attach(function(client, bufnr)
   -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  -- vim.keymap.set("i", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
+vim.diagnostic.config({
+    virtual_text = true
+})
--- .config/nvim/lua/abishevs/remap.lua
+++ .config/nvim/lua/abishevs/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
--- vim.keymap.set("n", "<leader>E", vim.cmd.NvimTreeFindFile)
--- vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+
 vim.keymap.set("n", "<leader>pf", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>o", vim.cmd.MarkdownPreview)
