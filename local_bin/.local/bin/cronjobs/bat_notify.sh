a647de388c76263fedaf2b7108e9bfa4a7b0f77a
Abishevs <eduards@abishevs.dev>
2024-12-28 12:13:41 +0100

    Update: new scripts + lf config

--- local_bin/.local/bin/cronjobs/bat_notify.sh
+++ local_bin/.local/bin/cronjobs/bat_notify.sh
@@ -8,9 +8,12 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 
 # Get the current battery percentage
 BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+# Get the charging status
+BATTERY_STATUS=$(acpi -b | grep -o 'Charging\|Discharging')
+notif-send -i "${BATTERY_STATUS}"
 
-# Check if the battery level is less than or equal to the threshold
-if [ "$BATTERY_LEVEL" -le "$THRESHOLD" ]; then
-	notify-send -i battery-caution "Battery Low" "Battery level is at ${BATTERY_LEVEL}%"
+# Check if the battery level is less than or equal to the threshold and if it is discharging
+if [ "$BATTERY_LEVEL" -le "$THRESHOLD" ] && [ "$BATTERY_STATUS" == "Discharging" ]; then
+    notify-send -i battery-caution "Battery Low" "Battery level is at ${BATTERY_LEVEL}%"
 fi
 
